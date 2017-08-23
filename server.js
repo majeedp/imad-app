9acc34f679a33746d2a71bec4544690b48a6e66e
majeedp <esteemservice@gmail.com>
2017-08-23 09:17:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('article,function (req,res){
+    res,send('article one requested will be send')
+}
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
