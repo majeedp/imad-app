759853c61b1781c679fe01cf45cbf20e52c6a4f6
majeedp <esteemservice@gmail.com>
2017-08-23 14:27:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,12 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
-    
-}
-
-
-    app.get('/ui/style.css', function (req, res) {});
+app.get('/ui/style.css', function (req, res) {});
 }
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
