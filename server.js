2af54392c3dc9d1791c252696e09429fa51c571f
majeedp <esteemservice@gmail.com>
2017-08-23 14:23:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,16 +9,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article one,function(req,res');{
-    res.send('article one requested and will bw served here');
+
+    
 }
 
-app.get('/article two,function(req,res');{
-    res.send('article two requested and will bw served here');
-}
 
-app.get('/article three,function(req,res');{
-    res.send('article three requested and will bw served here');
     app.get('/ui/style.css', function (req, res) {});
 }
 app.get('/ui/madi.png', function (req, res) {
